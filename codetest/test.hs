d887ce587c80ac3d0fae08c545cf22ab3c08ffff
sharyari <sharyari@gmail.com>
2014-06-23 01:21:55 +0200

    Added some functionality for traces, how to use it?

--- codetest/test.hs
+++ codetest/test.hs
@@ -1,12 +1,28 @@
 import Data.List
 import Data.Set
 import Data.Char
-data C = Conf [Int] [[Char]] C| Null    deriving (Show, Eq, Ord)
+data C = Conf [Int] [[Char]] C| Null
 data R = Rule [(Int, Int, Int)] [(Int, [Char], [Char])]
 -- Att göra senare: Titta på "boundary retransmission"
 -- Sliding window är beskrivet av tannenbaum
 
 --set 5: 3.2 6: 7.6, 7: 16.4
+
+instance Eq C where
+  (Conf a b c) == (Conf d e f) = ((a==d) && (b==e))
+  (Null) == (Null) = True
+  _ == _ = False
+
+instance Ord C where
+  (Conf s1 s2 _) `compare` (Conf s3 s4 _) = ((s1,s2) `compare` (s3,s4))
+  (Null) `compare` b = Prelude.LT
+  a `compare` (Null) = Prelude.GT
+
+
+instance Show C where
+  show (Conf ints chars parent) =
+    show ints ++ show chars ++ "\t\t" ++ show parent ++ "\n"
+  show (Null) = ""
 
 if' :: Bool -> a -> a -> a
 if' True  x _ = x
@@ -30,7 +46,7 @@ cartProd :: [[Char]] -> [[Char]] -> [[[Char]]]
 cartProd l1 l2 = sequence [cartProd' x symbols | x <- l1]
 
 longerwords' :: C -> Set C
-longerwords' (Conf sl chl) = fromList ([Conf sl x | x <- (cartProd chl symbols)])
+longerwords' (Conf sl chl parent) = fromList ([Conf sl x (Conf sl chl Null) | x <- (cartProd chl symbols)])
 longerwords :: Set C -> Set C
 longerwords l = unionMap longerwords' l
 
@@ -51,8 +67,8 @@ gammaD l k = Data.Set.map (gammaC l k) (longerwords l)
 
 alpha :: Int -> C -> (Set C)
 alpha _ Null = empty
-alpha k (Conf sl chl) =
-    fromList [Conf sl x | x <- (sequence (Data.List.map (subwordK k) chl))]
+alpha k (Conf sl chl parent) =
+    fromList [Conf sl x parent | x <- (sequence (Data.List.map (subwordK k) chl))]
 
 
 alphaL :: (Set C) -> Int -> (Set C)
@@ -61,8 +77,8 @@ alphaL l k =(unions [alpha k  x | x <- (toList l)])
 
 
 alg' :: (Set C) -> [R] -> Int -> (Set C)
---alg' l r k = Data.Set.union (alphaL (apply ((gammaD l k)) r) k)  l
-alg' l r k = alphaL (apply ((gammaD l k)) r) k
+alg' l r k = Data.Set.union l (alphaL (apply ((gammaD l k)) r) k)
+--alg' l r k = alphaL (apply ((gammaD l k)) r) k
 
 
 alg :: (Set C) -> [R] -> Int -> Int ->(Set C)
@@ -75,6 +91,17 @@ alg l r k counter =
     (alg a r k (counter-1))
 
 
+getParent :: C -> C
+getParent Null = Null
+getParent (Conf a b c ) = c
+
+findConf :: C -> [C] -> C
+findConf c [] = Null
+findConf c (h:tl) = if (c==h) then h else findConf c tl
+
+trace :: C -> [C] -> [C]
+trace Null list = []
+trace c list = c:trace (getParent (findConf c list)) list
 
 --runProgram :: [C] -> Int -> [C]
 --runProgram l 0 = l
@@ -100,33 +127,37 @@ changeChannel (chanNum, "?", symbol) channels =
   then replaceNth (chanNum-1) (tail ch) channels else []
 
 
+changeConf' :: R -> C -> C -> C
+changeConf' _ Null _ = Null
+changeConf' (Rule _ _) (Conf _ [] _) _ = Null
+changeConf' (Rule _ _) (Conf [] _ _) _ = Null
+changeConf' (Rule [] []) (Conf a b _) parent = (Conf a b parent)
+changeConf' (Rule [] (h:channelRules)) (Conf states channels p) parent =
+  changeConf' (Rule [] channelRules) (Conf states (changeChannel h channels) Null) parent
+changeConf' (Rule (h:stateRules) channelRules) (Conf states channels p) parent =
+  changeConf' (Rule stateRules channelRules) (Conf (changeState h states) channels Null) parent
+
 changeConf :: R -> C -> C
-changeConf _ Null = Null
-changeConf (Rule _ _) (Conf _ []) = Null
-changeConf (Rule _ _) (Conf [] _) = Null
-changeConf (Rule [] []) c = c
-changeConf (Rule [] (h:channelRules)) (Conf states channels) =
-  changeConf (Rule [] channelRules) (Conf states (changeChannel h channels))
-changeConf (Rule (h:stateRules) channelRules) (Conf states channels) =
-  changeConf (Rule stateRules channelRules) (Conf (changeState h states) channels)
+changeConf r (Conf a b parent) = changeConf' r (Conf a b Null) parent
+changeConf r Null = Null
+
+checkForBad [] (Conf _ _ _)  = True
+checkForBad _ (Conf [] _ _) = True
+checkForBad ((a,b):bsl) (Conf il chl parent) = if (il!!(a-1)==b) then (checkForBad bsl (Conf il chl parent)) else False
 
 
 
-
-checkForBad [] (Conf _ _)  = True
-checkForBad _ (Conf [] _ ) = True
-checkForBad ((a,b):bsl) (Conf il chl) = if (il!!(a-1)==b) then (checkForBad bsl (Conf il chl)) else False
-
-
-
-kor2 = (alg (fromList initial) transitions 5 50)
+kor2 = (alg (fromList initial) transitions 2 50)
 kor = Data.Set.filter (checkForBad bad) (alg (fromList initial) transitions 5 50)
-main = skriv (size kor2)
+--main = skriv (kor2)
+main = skriv (trace (Conf [4,4,2] ["bb","aa"] Null) (toList kor2))
 --main = skriv (changeConf r2 (head initial))
 --main = skriv (checkRule l2 (Conf [4,3] ["bbb", "aaa"]))
 --main = skriv (confToTuples (checkRule s1 (checkRule s0 (head initial))))
---main = skriv (apply (gammaC (fromList initial) (longerwords (fromList initial)) 2) transitions)
-
+--main = skriv (Data.Set.map (gammaC (fromList initial) 2) (longerwords (fromList initial)))
+--main = skriv (alphaL (apply (fromList initial) transitions) 2)
+--main = skriv (gammaC (fromList initial) 2 (head initial))
+--main = skriv ((alpha 2 (head initial)) `Data.Set.isSubsetOf` (fromList initial))
 symbols :: [[Char]]
 symbols = ["a","b",""]
 
@@ -154,6 +185,6 @@ sync6 = Rule [(3,1,3),(2,2,3)] []
 sync7 = Rule [(3,1,3),(2,4,1)] []
 
 
-initial = [Conf [1,1,1] ["",""]]
+initial = [Conf [1,1,1] ["",""] Null]
 transitions = [s1,s2,s3,s5,s6,s7,r0,r1,r2,r4,r5,r6,sync0,sync1,sync2,sync3,sync4,sync5,sync6,sync7]
 
