f9c428913c6e253cb3875893e28ee68c2d222254
sharyari <sharyari@gmail.com>
2014-08-14 20:45:40 +0200

    Changed MakeFile to take a .hs file as input, thus removing the need to move it to ProblemFormulation.hs by hand.

--- verifier/ABP.hs
+++ verifier/ABP.hs
@@ -32,7 +32,7 @@ sync5 = ([(2,2,1),(1,4,1)], (1,"_",[1]))
 sync6 = ([(2,1,3),(1,2,3)], (1,"_",[1]))
 sync7 = ([(2,1,3),(1,4,1)], (1,"_",[1]))
 
-transitions :: [([(Int,Int,Int)], (Int,String,[Int]))]
+transitions :: [([(Int,Int,Int)], (Int,String,CWord))]
 transitions = [s1,s2,s3,s5,s6,s7,r0,r1,r2,r4,r5,r6,sync0,sync1,sync2,sync3,sync4,sync5,sync6,sync7]
 
 
