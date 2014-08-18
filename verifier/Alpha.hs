9f111201957d5629c83b50fa2824c0307565bf9a
sharyari <sharyari@gmail.com>
2014-08-18 20:30:38 +0200

    Made an effectivization

--- verifier/Alpha.hs
+++ verifier/Alpha.hs
@@ -5,13 +5,13 @@ import TrieModule
 import StringManipulation
 import UnOrdered
 import Data.List as L
-
+import Data.Ord
 
 -- This is alpha. Alpha just calls alpha'. All this function does is to abstract internal
 -- ugliness from the outside world.
 alpha :: (CMap, CMap, [C]) -> Int -> (CMap,[C],CMap)
 alpha (confs, seen, list) k =
-  alpha' list k (confs,[],seen)
+  alpha' (sortBy (comparing fst) list) k (confs,[],seen)
 
 -- This is the actual alpha function. It will look at the list of new configuration, and take
 -- out all with the same state, and the send them to the function addViews which will calculate
@@ -21,18 +21,17 @@ alpha' [] k (confs,new,seen) = (confs,new,seen)
 alpha' (x:xs) k (confs,new,seen) =
   let
     state = fst x
-    (relevant',irrelevant) = L.partition (\y -> (fst y) == (fst x)) (x:xs)
-    (newConfs, new') = addViews' (L.map snd relevant') k (findNodeInMap state confs, [])
+    relevant' = L.map snd $ L.takeWhile (\y -> (fst y) == (fst x)) (x:xs)
+    newConfs = addViews' relevant' k S.empty
   in
-    alpha' irrelevant k
-    (mapAddNode confs state newConfs, L.zip (takeRepeat state) new' ++ new, seen) -- This line would fail if there would be more than 1M equal-state
+    alpha' (L.dropWhile (\y -> (fst y) == (fst x)) (x:xs)) k
+    (mapAddNode confs state newConfs, L.zip (takeRepeat state) (S.toList newConfs) ++ new, seen) -- This line would fail if there would be more than 1M equal-state
 
 -- This function finds the views of a Configuration and adds them to a set
-addViews' :: [Eval] -> Int -> (MapNode,[Eval]) -> (MapNode,[Eval])
-addViews' [] k (node,new) = (node, new)
-addViews' (x:chans) k (node,new) =
-  let newViews = views node k x in
-  addViews' chans k (S.union node $ S.fromList $ newViews, newViews++new)
+addViews' :: [Eval] -> Int -> MapNode -> MapNode
+addViews' [] k node = node
+addViews' (x:chans) k node =
+    addViews' chans k (S.union node $ S.fromList $ views node k x)
 
 
 
