35292445b034786b1cbcc1bc85e4555a1d5e7817
sharyari <sharyari@gmail.com>
2014-08-18 21:06:16 +0200

    Made it a little bit faster even

--- verifier/Alpha.hs
+++ verifier/Alpha.hs
@@ -6,26 +6,29 @@ import StringManipulation
 import UnOrdered
 import Data.List as L
 import Data.Ord
+import Data.HashMap.Strict as M
 
 -- This is alpha. Alpha just calls alpha'. All this function does is to abstract internal
 -- ugliness from the outside world.
 alpha :: (CMap, CMap, [C]) -> Int -> (CMap,[C],CMap)
 alpha (confs, seen, list) k =
-  alpha' (sortBy (comparing fst) list) k (confs,[],seen)
+  let new = alpha' (sortBy (comparing fst) list) k in
+  (M.unionWith (S.union) confs (M.fromList new), L.concat $ L.map help new, seen)
+
+help (a,b) = [(a,x) | x <- S.toList b]
 
 -- This is the actual alpha function. It will look at the list of new configuration, and take
 -- out all with the same state, and the send them to the function addViews which will calculate
 -- their views and add them to a set. Then, alpha' merges that with the old set.
-alpha' :: [C] -> Int -> (CMap, [C], CMap) -> (CMap,[C],CMap)
-alpha' [] k (confs,new,seen) = (confs,new,seen)
-alpha' (x:xs) k (confs,new,seen) =
+alpha' :: [C] -> Int -> [(State,MapNode)]
+alpha' [] k = []
+alpha' xs k =
   let
-    state = fst x
-    relevant' = L.map snd $ L.takeWhile (\y -> (fst y) == (fst x)) (x:xs)
-    newConfs = addViews' relevant' k S.empty
+    state = (fst $ head xs)
   in
-    alpha' (L.dropWhile (\y -> (fst y) == (fst x)) (x:xs)) k
-    (mapAddNode confs state newConfs, L.zip (takeRepeat state) (S.toList newConfs) ++ new, seen) -- This line would fail if there would be more than 1M equal-state
+    (state, addViews' (L.map snd $ L.takeWhile (\y -> (fst y) == state) xs) k S.empty):
+    (alpha' (L.dropWhile (\y -> (fst y) == state) xs) k)
+    
 
 -- This function finds the views of a Configuration and adds them to a set
 addViews' :: [Eval] -> Int -> MapNode -> MapNode
